1aa06c616af9c8ce882c0c7da6ffdbecc26dac04
ISMashtakov <ismashtakov@edu.hse.ru>
2022-05-31 14:38:28 +0300

    fix tests

--- CodeDocs_backend/settings.py
+++ CodeDocs_backend/settings.py
@@ -164,14 +164,11 @@ WSGI_APPLICATION = 'CodeDocs_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'code_docs',
+        'NAME': 'shared_boards',
         'USER': 'postgres',
-        'PASSWORD': 'password',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
-        'TEST': {
-               'NAME': 'auto_tests',
-           }
     }
 }
 
