5604e60807ba2a62053d805ea4d762b9218a30da
ISMashtakov <ismashtakov@edu.hse.ru>
2022-06-04 12:51:22 +0300

    add owner

--- board_manager/views.py
+++ board_manager/views.py
@@ -49,7 +49,7 @@ def my_boards(request):
     boards = BoardManager.get_user_boards(request.user)
     serializer = UserBoardsSerializer(boards, many=True)
     for row in serializer.data:
-        owner = UserBoards.objects.get(board=row["id"], access=Access.OWNER)
+        owner = UserBoards.objects.get(board=row["board"]["id"], access=Access.OWNER)
         row["owner"] = UserSerializer(owner).data
     return JsonResponse(serializer.data, status=status.HTTP_200_OK, safe=False)
 
