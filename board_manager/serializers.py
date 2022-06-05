5a166f8bdc746ecf9916221b103841dc4bb047f9
ISMashtakov <ismashtakov@edu.hse.ru>
2022-06-06 00:51:18 +0300

    fix

--- board_manager/serializers.py
+++ board_manager/serializers.py
@@ -40,5 +40,5 @@ class NodeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Node
-        exclude = ['tag', 'board']
+        fields = ('position_x', 'position_y', 'full_tag')
 
