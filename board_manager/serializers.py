5cbaab660bb724fc771ff83925668881c025b171
ISMashtakov <ismashtakov@edu.hse.ru>
2022-06-07 02:12:41 +0300

    fix

--- board_manager/serializers.py
+++ board_manager/serializers.py
@@ -46,5 +46,5 @@ class NodeSerializer(serializers.ModelSerializer):
 class ColumnSerializer(serializers.ModelSerializer):
     class Meta:
         model = Column
-        fields = '__all__
+        fields = '__all__'
 
